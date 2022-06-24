01b56255afcf2ea4e5eaaf2f1d80b05afa002d02
Ethkuil <hobartlin23@gmail.com>
2022-06-25 07:29:11 +0800

    refactor(aplay): 删除调试语句

--- user/aplaycore.c
+++ user/aplaycore.c
@@ -90,9 +90,7 @@ void apShowAudioInfo(struct ApAudioPlayInfo *apinfo) {
 }
 
 int apReadDecode(struct ApAudioPlayInfo *apinfo) {
-  printf("in ap read decode\n");
-  
-  int pid = fork(); printf("pid: %d\n", pid);
+  int pid = fork();
   if (pid < 0) {
     fprintf(2, "fail to fork");
     return -1;
@@ -104,23 +102,21 @@ int apReadDecode(struct ApAudioPlayInfo *apinfo) {
   // 子进程
   int fd = apinfo->fd;
   char fileData[READ_BUFFER_SIZE];
-  fd = fd; fileData[0] = fileData[0];
   if (apinfo->ftype == WAV) {
     while (1) {
       int nRead = read(fd, fileData, READ_BUFFER_SIZE);
-      printf("nRead: %d\n", nRead);
       if (nRead == 0) exit(0);
       writeDecodedAudio(fileData, nRead);
     }
   }
   /* 处理其他音频格式
+    #define DEC_BUFFER_SIZE 100
     void (*decode)(const char *fileData, char *decodedData) = 0;
     switch (apinfo->ftype) {
       case MP3:
         decode = decodeMp3;
         break;
     }
-  #define DEC_BUFFER_SIZE 100
     char decodedData[DEC_BUFFER_SIZE];
     while (1) {
       int nRead = read(fd, fileData, READ_BUFFER_SIZE);
